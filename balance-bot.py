5712a49a1203798cef05443308108f94431b96fb
Brian Burton <brian-burton@users.noreply.github.com>
2018-02-21 10:13:28 +0000

    Syntax fix

--- balance-bot.py
+++ balance-bot.py
@@ -8,7 +8,7 @@ BASE_X, BASE_Y, BASE_Z = motion.accelerometer()
 try:
   while True:
     x, y, z = motion.accelerometer()
-    print(round(100*y)
+    print(round(100*y))
     sleep(0.2)
 except KeyboardInterrupt:
   motor.stop()
