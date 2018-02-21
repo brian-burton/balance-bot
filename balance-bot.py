3560d9f755bc9624d51cff7e4461930a7c761877
Brian Burton <brian-burton@users.noreply.github.com>
2018-02-21 10:19:35 +0000

    First test with real motors

--- balance-bot.py
+++ balance-bot.py
@@ -8,8 +8,10 @@ BASE_X, BASE_Y, BASE_Z = motion.accelerometer()
 try:
   while True:
     x, y, z = motion.accelerometer()
-    print(round(100*y))
-    sleep(0.2)
+    delta_angle = round(100*(y-BASE_Y))
+    motor.speed(delta_angle)
+    print(delta_angle)
+    sleep(0.1)
 except KeyboardInterrupt:
   motor.stop()
   sys.exit()
