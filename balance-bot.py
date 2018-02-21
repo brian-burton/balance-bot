8cb1e242ca67972fb2193dd3048cf6a3aa55ce9b
Brian Burton <brian-burton@users.noreply.github.com>
2018-02-21 10:35:22 +0000

    Change to forwards/backwards instead of speed

--- balance-bot.py
+++ balance-bot.py
@@ -10,8 +10,14 @@ try:
   while True:
     x, y, z = motion.accelerometer()
     delta_angle = round(100*(y-BASE_Y))
+    if delta_angle < 0:
+      motor.backwards(max(-100, 2*delta_angle))
+    elif delta_angle > 0:
+      motor.forwards(min(100,2*delta_angle))
+    else:
+      motor.stop()
     # clamp motor speed between -100 and 100
-    motor.speed(min(100,max(-100,2*delta_angle)))
+    # motor.speed(min(100,max(-100,2*delta_angle)))
     print(delta_angle)
     sleep(0.1)
 except KeyboardInterrupt:
