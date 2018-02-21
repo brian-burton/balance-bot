b6f44b321c6d71d3e73f33d2dbffe98258760abd
Brian Burton <brian-burton@users.noreply.github.com>
2018-02-21 10:28:26 +0000

    Change right motor direction; increase speed

--- balance-bot.py
+++ balance-bot.py
@@ -4,12 +4,14 @@ from envirophat import motion
 from explorerhat import motor
 
 BASE_X, BASE_Y, BASE_Z = motion.accelerometer()
+motor.two.invert()
 
 try:
   while True:
     x, y, z = motion.accelerometer()
     delta_angle = round(100*(y-BASE_Y))
-    motor.speed(delta_angle)
+    # clamp motor speed between -100 and 100
+    motor.speed(min(100,max(-100,2*delta_angle)))
     print(delta_angle)
     sleep(0.1)
 except KeyboardInterrupt:
